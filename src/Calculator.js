98bb216a117ff23fa9e19caa7ba4fe871d4f8ea9
kendalwon <kendalwon@gmail.com>
2020-04-02 13:33:14 -0400

    add shortcuts, big fix

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './Calculator.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDivide } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faMinus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faEquals } from '@fortawesome/free-solid-svg-icons';
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -44,7 +50,43 @@ class Calculator extends React.Component {
       this.setState(prevState => ({
         temp: `${prevState.temp}${input}`
       }));
-    } 
+    } else if (e.keyCode === 68) {
+      const input1 = this.state.temp;
+      this.setState({
+        temp: '',
+        input1: input1,
+        operation: 'divide'
+      });
+    } else if (e.keyCode === 84) {
+      const input1 = this.state.temp;
+      this.setState({
+        temp: '',
+        input1: input1,
+        operation: 'multiply'
+      });
+    } else if (e.keyCode === 77) {
+      const input1 = this.state.temp;
+      this.setState({
+        temp: '',
+        input1: input1,
+        operation: 'subtract'
+      });
+    } else if (e.keyCode === 80) {
+      const input1 = this.state.temp;
+      this.setState({
+        temp: '',
+        input1: input1,
+        operation: 'add'
+      });
+    } else if (e.keyCode === 69 || e.keyCode === 13) {
+      const input2 = this.state.temp;
+      this.setState({
+        temp: '',
+        input2: input2
+      }); 
+    } else if (e.keyCode === 67) {
+      this.clear(); 
+    } else return;
   }
 
   handleDigitClick(e) {
@@ -110,6 +152,7 @@ class Calculator extends React.Component {
   }
   
   render() {
+    console.log(this.state);
     return (
       <div className='body'>
         <div className='container'>
@@ -124,7 +167,7 @@ class Calculator extends React.Component {
             <button className='operation divide'
               value='divide'
               onClick={this.handleOperationClick}>
-                /
+              <FontAwesomeIcon icon={faDivide} />
             </button>
             <button className='digit seven'
               value='7'
@@ -144,7 +187,7 @@ class Calculator extends React.Component {
             <button className='operation multiply'
               value='multiply'
               onClick={this.handleOperationClick}>
-                x
+              <FontAwesomeIcon icon={faTimes} />
             </button>
             <button className='digit four'
               value='4'
@@ -164,7 +207,7 @@ class Calculator extends React.Component {
             <button className='operation subtract'
               value='subtract'
               onClick={this.handleOperationClick}>
-                -
+              <FontAwesomeIcon icon={faMinus} />
             </button>
             <button className='digit one'
               value='1'
@@ -184,7 +227,7 @@ class Calculator extends React.Component {
             <button className='operation add'
               value='add'
               onClick={this.handleOperationClick}>
-                +
+              <FontAwesomeIcon icon={faPlus} />
             </button>
             <button className='digit zero'
               value='0'
@@ -194,10 +237,36 @@ class Calculator extends React.Component {
             <button className='operation equals'
               value='='
               onClick={this.handleEqualsClick}>
-                =
+              <FontAwesomeIcon icon={faEquals} />
             </button>
           </div>
-        </div>   
+        </div>
+        <div className='instructions'>
+          <ul className='shortcutList'>Keyboard Shortcuts:
+            <li className='shortcut'>
+              digits as they appear
+            </li>
+            <li className='shortcut'>
+              'p' for plus
+            </li>
+            <li className='shortcut'>
+              'm' for minus
+            </li>
+            <li className='shortcut'>
+              't' for times
+            </li>
+            <li className='shortcut'>
+              'd' for divided by
+            </li>
+            <li className='shortcut'>
+              'e' for equals
+            </li>
+            <li className='shortcut'>
+              'c' for clear
+            </li>
+          </ul> 
+        </div>
+        
       </div>  
     );
   }
